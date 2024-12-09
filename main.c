7757f16fdfc772df425862d0d2864f0f262e6416
Abdulloh20080825 <abdullohqurbonov332@gmail.com>
2024-12-09 22:20:28 +0500

    # 4 if elseif else

--- main.c
+++ main.c
@@ -2,25 +2,20 @@
 
 int main()
 {
-  // empty array must have a 5 elements !!
-  int arr[] = {4, 23, 2, 4, 3};
-  arr[0] = 5;
+  int x = 3;
 
-  printf("%d\n", arr[3]);
-
-  float flArr[3];
-  flArr[0] = 32.3f;
-  flArr[1] = 335.3f;
-  flArr[2] = 32.3f;
-
-  char word[] = {'S', 'o', 'm', 'e'};
-  char lng[] = "Hello World";
-
-  printf("%s\n", lng);
-
-  // Create matrix
-  int matrix[2][3] = {
-      {12, 32, 23}, {32, 32, 32, 32}};
+  if (x == 5)
+  {
+    printf("Yes\n");
+  }
+  else if (x == 7)
+  {
+    printf("Number is 7");
+  }
+  else
+  {
+    printf("Error");
+  }
 
   return 0;
 }
