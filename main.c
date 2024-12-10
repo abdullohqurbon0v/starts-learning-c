b74e239f741edd54e42c3ea0887d3a6f9aae7f61
Abdulloh20080825 <abdullohqurbonov332@gmail.com>
2024-12-10 17:29:01 +0500

    # 5 for while do while loops

--- main.c
+++ main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-  int x = 3;
 
-  if (x == 5)
-  {
-    printf("Yes\n");
-  }
-  else if (x == 7)
-  {
-    printf("Number is 7");
-  }
-  else
-  {
-    printf("Error");
-  }
+     // for (float i = 100; i > 10; i /= 2)
+     // {
+     //      printf("%2f\n", i);
+     // }
 
-  return 0;
+     // bool isHasCar = true;
+     // int x = 0;
+     // char carName[20];
+
+     // while (isHasCar)
+     // {
+     //      printf("Enter a car name: ");
+     //      scanf("%s", &carName);
+     //      if (x)
+     //      {
+     //           break;
+     //      }
+     //      x++;
+     // }
+
+     // printf("%s", carName);
+
+     // int i = 0;
+
+     // do
+     // {
+     //      printf("Hello World\n");
+     //      i++;
+     // } while (i < 10);
+     // printf("Hello World");
+
+     int arr[] = {3, 53, 23, -32, 3};
+     for (int i = 0; i < 5; i++)
+     {
+          printf("%d\n", arr[i]);
+     }
+     return 0;
 }
